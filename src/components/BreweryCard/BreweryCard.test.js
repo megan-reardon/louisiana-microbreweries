6323f017a4c9550cc6191499cded031f4f2a4332
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-16 12:12:50 -0600

    test for rendering BreweryCard

---
+++ src/components/BreweryCard/BreweryCard.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import BreweryCard from './BreweryCard.js';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { rootReducer } from '../../reducers';
+import '@testing-library/jest-dom';
+
+describe('BreweryCard', () => {
+  it('should render the brewery information on each brewery card', () => {
+    const testStore = createStore(rootReducer);
+    const testWrapper = <Provider store={testStore}>
+    <BreweryCard
+      name={'Parleaux Beer Lab'}
+      city={'New Orleans'}
+    />
+    </Provider>;
+
+    const { getByText, getByAltText } = render(testWrapper)
+
+    const nameEl = getByText('Parleaux Beer Lab');
+    const cityEl = getByText('New Orleans, LA');
+    const altText = getByAltText('Logo for Parleaux Beer Lab');
+
+    expect(nameEl).toBeInTheDocument();
+    expect(cityEl).toBeInTheDocument();
+    expect(altText).toBeInTheDocument();
+  })
+})
