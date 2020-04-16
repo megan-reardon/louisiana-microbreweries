a376f68d0e6dd082333a804b9d9cef596a2658ef
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-16 11:52:57 -0600

    styling brewery cards

--- src/components/BreweryCard/BreweryCard.js
+++ src/components/BreweryCard/BreweryCard.js
@@ -5,13 +5,15 @@ import PropTypes from 'prop-types';
 const BreweryCard = ({ id, name, city }) => {
   return (
     <div className='brewery-card'>
-      <h2>
-      {name}
-      </h2>
-      <h2>
-      {city}
-      </h2>
       <img src={`../../../logos/${id}.png`} />
+      <div className='brewery-city-container'>
+        <h2>
+        {name}
+        </h2>
+        <h1>
+        {city}
+        </h1>
+      </div>
     </div>
   )
 }
