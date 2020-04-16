1fe24c8d061e4c53b1f193a5620943272700897d
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-16 14:55:08 -0600

    added route to brewery listing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getBreweries } from '../../actions';
 import PropTypes from 'prop-types';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import Nav from '../Nav/Nav';
 import BreweryContainer from '../BreweryContainer/BreweryContainer';
+import BreweryDetails from '../BreweryDetails/BreweryDetails';
 import { apifetchBreweries } from '../../apiCalls';
 
 class App extends Component {
@@ -20,7 +21,24 @@ class App extends Component {
     return (
       <main>
         <Nav />
-        <BreweryContainer />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <BreweryContainer />
+            )}
+          />
+          <Route
+            path="/breweries/:id/" exact
+            render={({ match }) => {
+              const selectedBrewery = this.props.brewery.find(brewery => parseInt(match.params.id) === brewery.id)
+              return <BreweryDetails
+                     {...selectedBrewery}
+                     />
+            }}
+          />
+        </Switch>
       </main>
     )
   }
