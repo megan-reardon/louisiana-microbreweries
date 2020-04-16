d0b3fb83f7cc220a7417fba59c4bd6f95c6780ee
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-15 18:17:30 -0600

    added fetch for breweries

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,8 +4,15 @@ import PropTypes from 'prop-types';
 import { Route } from 'react-router-dom';
 
 import Nav from '../Nav/Nav';
+import { apifetchBreweries } from '../apiCalls'
 
 class App extends Component {
+
+  componentDidMount = () => {
+    apifetchBreweries()
+    .then(data => this.props.fetchBreweries(data.breweries))
+    .catch(err => console.log(err.message))
+  }
 
   render() {
     return (
@@ -15,5 +22,9 @@ class App extends Component {
     )
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchBreweries: breweries => dispatch( getBreweries(breweries))
+})
 
 export default App;
