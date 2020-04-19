276887ced7ec55c7e9832134da5890aeea011c2e
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-19 12:40:48 -0600

    added action tests for add and remove favorite

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -12,5 +12,31 @@ describe('Actions Tests', () => {
     const result = actions.getBreweries([brewery1, brewery2])
 
     expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of ADD_FAVORITE, and a correct payload of favorites', () => {
+    const favorite1 = {id: 2941, name: 'Crying Eagle Brewing Company'};
+    const favorite2 = {id: 2944, name: 'Flying Heart Brewing'};
+
+    const expectedAction = {
+      type: 'ADD_FAVORITE',
+      favorites: [favorite1, favorite2]
+    }
+    const result = actions.addFavorite([favorite1, favorite2])
+
+    expect(result).toEqual(expectedAction);
+  });
+
+  it('should have a type of REMOVE_FAVORITE, and a correct payload of favorite', () => {
+    const favorite1 = {id: 2941, name: 'Crying Eagle Brewing Company'};
+    const favorite2 = {id: 2944, name: 'Flying Heart Brewing'};
+
+    const expectedAction = {
+      type: 'REMOVE_FAVORITE',
+      favorite: [favorite1]
+    }
+    const result = actions.removeFavorite([favorite1])
+
+    expect(result).toEqual(expectedAction);
   })
 })
