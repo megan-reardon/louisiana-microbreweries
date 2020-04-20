73b21839dd42a3c56e9ee2c8d4b3d9ee0fb0d1a0
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-20 13:32:01 -0600

    rewriting integration tests without beforeEach

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -10,81 +10,136 @@ import { apifetchBreweries } from '../../apiCalls';
 jest.mock('../../apiCalls');
 
 describe('App', () => {
-  let utils;
-  let store;
-  let fetchedBreweries;
-
-  beforeEach(() => {
-    fetchedBreweries =
-      [
-        {id: 2941,
-        name: "Crying Eagle Brewing Company",
-        brewery_type: "micro",
-        street: "1165 E McNeese St",
-        city: "Lake Charles",
-        state: "Louisiana",
-        postal_code: "70607-4753",
-        phone: "3379904871",
-        website_url: "http://www.cryingeagle.com"},
-        {
-        id: 2944,
-        name: "Flying Heart Brewing",
-        brewery_type: "micro",
-        street: "700 Barksdale Blvd",
-        city: "Bossier City",
-        state: "Louisiana",
-        postal_code: "71111-4502",
-        phone: "3183448775",
-        website_url: "http://www.flyingheartbrewing.com",
-        }
-      ]
-
-    store = createStore(rootReducer);
-    utils =
+  it('Can view the breweries when the app loads', async() => {
+    const store = createStore(rootReducer);
+    const utils =
       <Provider store={store}>
         <Router>
           <App
-            // currentPage={'favorites'}
-            // favorites={[]}
           />
         </Router>
       </Provider>;
-  });
 
-  afterEach(() => {
-    cleanup
-  });
+    const fetchedBreweries =
+    [
+      {id: 2941,
+      name: "Crying Eagle Brewing Company",
+      street: "1165 E McNeese St",
+      city: "Lake Charles",
+      state: "Louisiana",
+      postal_code: "70607-4753",
+      phone: "3379904871",
+      website_url: "http://www.cryingeagle.com"},
+      {
+      id: 2944,
+      name: "Flying Heart Brewing",
+      street: "700 Barksdale Blvd",
+      city: "Bossier City",
+      state: "Louisiana",
+      postal_code: "71111-4502",
+      phone: "3183448775",
+      website_url: "http://www.flyingheartbrewing.com",
+      }
+    ]
 
-  it('Can view the breweries when the app loads', async() => {
     apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
-    const { getByText } = render(utils);
-    const breweryName = await waitForElement(() => getByText('Crying Eagle Brewing Company'))
-    const cityEl = await waitForElement(() => getByText('Bossier City, LA'))
+    const { getByText, getByTestId } = render(utils);
+    const breweryName = await waitForElement(() => getByText('Crying Eagle Brewing Company'));
+    const cityEl = await waitForElement(() => getByText('Bossier City, LA'));
+    const breweryContainer = await waitForElement(() => getByTestId('brewery-container'));
 
     expect(breweryName).toBeInTheDocument();
     expect(cityEl).toBeInTheDocument();
-  });
+    expect(breweryContainer).toBeInTheDocument();
 
-  it('should be able to navigate to the brewery details page on click of a specific brewery card', async () => {
+});
+
+  it('Can view the breweries when the app loads', async() => {
+      const store = createStore(rootReducer);
+      const utils =
+        <Provider store={store}>
+          <Router>
+            <App
+            />
+          </Router>
+        </Provider>;
+
+    const fetchedBreweries =
+    [
+      {id: 2941,
+      name: "Crying Eagle Brewing Company",
+      street: "1165 E McNeese St",
+      city: "Lake Charles",
+      state: "Louisiana",
+      postal_code: "70607-4753",
+      phone: "3379904871",
+      website_url: "http://www.cryingeagle.com"},
+      {
+      id: 2944,
+      name: "Flying Heart Brewing",
+      street: "700 Barksdale Blvd",
+      city: "Bossier City",
+      state: "Louisiana",
+      postal_code: "71111-4502",
+      phone: "3183448775",
+      website_url: "http://www.flyingheartbrewing.com",
+      }
+    ]
     apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
-    const { getByText,  getByTestId } = render(utils);
-    await waitForElement(() => getByTestId('2944 container'));
+    const { getByText, getByTestId } = render(utils);
+    const breweryName = await waitForElement(() => getByText('Crying Eagle Brewing Company'));
+    const cityEl = await waitForElement(() => getByText('Bossier City, LA'));
+    const breweryContainer = await waitForElement(() => getByTestId('brewery-container'));
 
-    fireEvent.click(getByTestId('2944 container'))
-    const breweryPhone = await waitForElement(() => getByText("318-344-8775"));
-
-    expect(breweryPhone).toBeInTheDocument();
+    expect(breweryName).toBeInTheDocument();
+    expect(cityEl).toBeInTheDocument();
+    expect(breweryContainer).toBeInTheDocument();
   });
+
+  // it('should be able to navigate to the brewery details page on click of a specific brewery card', async () => {
+  //   apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
+  //   const { getByText,  getByTestId } = render(utils);
+  //   await waitForElement(() => getByTestId('2944 container'));
+  //
+  //   fireEvent.click(getByTestId('2944 container'))
+  //   const breweryPhone = await waitForElement(() => getByText("318-344-8775"));
+  //
+  //   expect(breweryPhone).toBeInTheDocument();
+  // });
 
   // it('should render a message of no favorites if a user views favorites but does not have any yet', async () => {
   //   apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
-  //   const { getByText } = render(utils);
+  //   const { getByText, debug } = render(utils);
   //   const favoriteBtn = getByText('View Saved Breweries');
-  //
+  //   debug()
   //   fireEvent.click(favoriteBtn);
   //
-  //   const errMessage = await waitForElement(() => getByText('You do not have any saved breweries'));
+  //   const errMessage = await waitForElement(() => getByText('You do not have any saved breweries yet!'));
   //
   //   expect(errMessage).toBeInTheDocument();
+  // })
+
+  // it('should show favorite breweries after a user has added them', async () => {
+  //
+  //   apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
+  //   const { getByText,  getByTestId } = render(utils);
+  //   await waitForElement(() => getByTestId('2941 container'));
+  //
+  //   fireEvent.click(getByTestId('2941 container'))
+  //   const breweryPhone = await waitForElement(() => getByText("337-990-4871"));
+  //
+  //   expect(breweryPhone).toBeInTheDocument();
+    // const viewSavedBtn = getByText('View Saved Breweries');
+
+    // fireEvent.click(breweryContainer);
+    //
+    // const saveBtn = await waitForElement(() => getByText('Save this Brewery'));
+
+    // fireEvent.click(saveBtn);
+    // fireEvent.click(viewSavedBtn);
+    //
+    // const cityEl = await waitForElement(() => getByText('Bossier City, LA'))
+    //
+    // expect(cityEl).toBeInTheDocument();
   // })
 })
