2ac6dc0a7eef77b35cfcfab141f6f3365f759968
Megan Venetianer <megan.venetianer@gmail.com>
2020-04-20 14:11:41 -0600

    test error message if there are no favorite breweries

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -95,49 +95,47 @@ describe('App', () => {
     fireEvent.click(getByText("Louisiana Micreauxbreweries"))
   });
 
+  it('should render a message of no favorites if a user views favorites but does not have any yet', async () => {
+    const store = createStore(rootReducer);
+    const utils =
+      <Provider store={store}>
+        <Router>
+          <App
+          />
+        </Router>
+      </Provider>;
 
-  //
-  // it('should render a message of no favorites if a user views favorites but does not have any yet', async () => {
-  //   const store = createStore(rootReducer);
-  //   const utils =
-  //     <Provider store={store}>
-  //       <Router>
-  //         <App
-  //         />
-  //       </Router>
-  //     </Provider>;
-  //
-  // const fetchedBreweries =
-  // [
-  //   {id: 2941,
-  //   name: "Crying Eagle Brewing Company",
-  //   street: "1165 E McNeese St",
-  //   city: "Lake Charles",
-  //   state: "Louisiana",
-  //   postal_code: "70607-4753",
-  //   phone: "3379904871",
-  //   website_url: "http://www.cryingeagle.com"},
-  //   {
-  //   id: 2944,
-  //   name: "Flying Heart Brewing",
-  //   street: "700 Barksdale Blvd",
-  //   city: "Bossier City",
-  //   state: "Louisiana",
-  //   postal_code: "71111-4502",
-  //   phone: "3183448775",
-  //   website_url: "http://www.flyingheartbrewing.com",
-  //   }
-  // ]
-  //   apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
-  //   const { getByText, debug } = render(utils);
-  //   const favoriteBtn = getByText('View Saved Breweries');
-  //   debug()
-  //   fireEvent.click(favoriteBtn);
-  //
-  //   const errMessage = await waitForElement(() => getByText('You do not have any saved breweries yet!'));
-  //
-  //   expect(errMessage).toBeInTheDocument();
-  // })
+  const fetchedBreweries =
+  [
+    {id: 2941,
+    name: "Crying Eagle Brewing Company",
+    street: "1165 E McNeese St",
+    city: "Lake Charles",
+    state: "Louisiana",
+    postal_code: "70607-4753",
+    phone: "3379904871",
+    website_url: "http://www.cryingeagle.com"},
+    {
+    id: 2944,
+    name: "Flying Heart Brewing",
+    street: "700 Barksdale Blvd",
+    city: "Bossier City",
+    state: "Louisiana",
+    postal_code: "71111-4502",
+    phone: "3183448775",
+    website_url: "http://www.flyingheartbrewing.com",
+    }
+  ]
+    apifetchBreweries.mockResolvedValueOnce(fetchedBreweries);
+    const { getByText } = render(utils);
+    const favoriteBtn = getByText('View Saved Breweries');
+  
+    fireEvent.click(favoriteBtn);
+
+    const errMessage = await waitForElement(() => getByText('You do not have any saved breweries yet!'));
+
+    expect(errMessage).toBeInTheDocument();
+  })
 
   it('should show favorite breweries after a user has added them', async () => {
     const store = createStore(rootReducer);
@@ -181,8 +179,8 @@ describe('App', () => {
 
     fireEvent.click(saveBtn);
     fireEvent.click(viewSavedBtn);
-    
+
     const cityEl = await waitForElement(() => getByText('Lake Charles, LA'))
     expect(cityEl).toBeInTheDocument();
-  })
+  });
 })
